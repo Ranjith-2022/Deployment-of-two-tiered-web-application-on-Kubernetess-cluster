3599d947e3d99469eefeba42c0faac861bd2327c
Krishna1423 <kammayisayesivakumaa@myseneca.ca>
2024-04-08 05:59:31 +0000

    Added env variables and conditions to check variables

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from flask import jsonify
+from flask import Flask, render_template, request
+from pymysql import connections
 import boto3
 import os
 import random
@@ -7,15 +9,44 @@ import argparse
 
 app = Flask(__name__)
 
-# Function to retrieve background image URL from ConfigMap
-def get_background_image_url():
-    # Replace this with your actual ConfigMap retrieval logic
-    return "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hcamag.com%2Fca%2Fspecialization%2Frecruitment%2Fwhere-are-the-best-places-to-work-in-toronto%2F449981&psig=AOvVaw1h3a-21n5jLMF4_e4FQgGK&ust=1712473430555000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMD3uuaCrYUDFQAAAAAdAAAAABAE"
+DBHOST = os.environ.get("DBHOST") or "localhost"
+DBUSER = os.environ.get("DBUSER") or "root"
+DBPWD = os.environ.get("DBPWD") or "passwors"
+DATABASE = os.environ.get("DATABASE") or "employees"
+COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
+DBPORT = int(os.environ.get("DBPORT"))
+BGIMAGE = os.environ.get("BGIMAGE") or "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hcamag.com%2Fca%2Fspecialization%2Frecruitment%2Fwhere-are-the-best-places-to-work-in-toronto%2F449981&psig=AOvVaw1h3a-21n5jLMF4_e4FQgGK&ust=1712473430555000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMD3uuaCrYUDFQAAAAAdAAAAABAE"
+OURNAMES = os.environ.get("OURNAMES")
 
-# Function to retrieve MySQL DB credentials from K8s secrets
-def get_mysql_credentials():
-    # Replace this with your actual K8s secrets retrieval logic
-    return {"username": "your-mysql-username", "password": "your-mysql-password"}
+
+# Create a connection to the MySQL database
+db_conn = connections.Connection(
+    host= DBHOST,
+    port=DBPORT,
+    user= DBUSER,
+    password= DBPWD, 
+    db= DATABASE
+    
+)
+output = {}
+table = 'employee';
+
+# Define the supported color codes
+color_codes = {
+    "red": "#e74c3c",
+    "green": "#16a085",
+    "blue": "#89CFF0",
+    "blue2": "#30336b",
+    "pink": "#f4c2c2",
+    "darkblue": "#130f40",
+    "lime": "#C1FF9C",
+}
+
+# Create a string of supported colors
+SUPPORTED_COLORS = ",".join(color_codes.keys())
+
+# Generate a random color
+COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
 
 @app.route("/")
 def home():
@@ -32,5 +63,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--color', required=False)
     args = parser.parse_args()
+    
+    if args.color:
+        print("Color from command line argument =" + args.color)
+        COLOR = args.color
+        if COLOR_FROM_ENV:
+            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
+    elif COLOR_FROM_ENV:
+        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
+        COLOR = COLOR_FROM_ENV
+    else:
+        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
+
+    # Check if input color is a supported one
+    if COLOR not in color_codes:
+        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
+        exit(1)
 
     app.run(host='0.0.0.0', port=81, debug=True)
