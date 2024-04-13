35d14c2630450336d62cb03e8ef780e10714136c
ashmy141 <ashmythomas123@gmail.com>
2024-04-13 13:19:11 +0000

    config files

--- Manifest_files/backend_files/backend.sh
+++ Manifest_files/backend_files/backend.sh
@@ -1,3 +1,5 @@
 k create -f secret-for-sqldb.yaml -n final
 k create -f pvc-for-sqldb.yaml -n final
 k get pvc -n final
+k create -f sqldb-pod-deployment.yaml -n final
+k create -f service-sqldb.yaml -n final
