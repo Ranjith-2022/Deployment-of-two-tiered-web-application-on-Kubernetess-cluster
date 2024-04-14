b425eac0510a4fcb69a6db07d39c0bfd388faf47
ashmy141 <ashmythomas123@gmail.com>
2024-04-14 01:48:48 +0000

    deleted backend.sh

--- Manifest_files/backend_files/backend.sh
+++
@@ -1,7 +0,0 @@
-k create -f secret.yaml -n final
-k create -f pvc.yaml -n final
-k get pvc -n final
-k create -f db-deployment.yaml -n final
-k create -f db-service.yaml -n final
-k get all -n final
-k get pvc -n final
